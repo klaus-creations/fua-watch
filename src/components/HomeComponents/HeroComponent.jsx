0f9285f41457d381e85e54db706f9c820ae1ab0b
Abush Natanem <abushnatanem@gmail.com>
2025-03-26 12:10:39 +0300

    update hero component

--- src/components/HomeComponents/HeroComponent.jsx
+++ src/components/HomeComponents/HeroComponent.jsx
@@ -22,20 +22,16 @@ function HeroComponent({ data }) {
 
 const HeroCenter = function ({ data }) {
   return (
-    <div className="w-[100%] sm:w-[90%] md:w-[75%] lg:w-[60%] 2xl:w-[40%] px-4 lg:px-6 flex items-center  absolute top-[10%] md:top-[15%] lg:top-[20%] left-[50%] md:left-[40%] lg:left-[30%] -translate-x-[50%] bg-indigo-950/[.3] z-20 py-4 md:py-8 lg:py-10   shadow-lg shadow-sky-700 flex-wrap">
+    <div className="w-[100%] sm:w-[90%] md:w-[75%] lg:w-[60%] 2xl:w-[40%] px-4 lg:px-6 flex items-center  absolute top-[10%] md:top-[15%] lg:top-[20%] left-[50%] md:left-[40%] lg:left-[30%] -translate-x-[50%] bg-indigo-950/[.3] z-20 py-4 md:py-8 lg:py-10   shadow-lg shadow-sky-700 flex-wrap flex-col">
       <img
-        className="w-[50%]"
+        className="w-[80%] lg:w-[50%]"
         src={tmdbImagePath(data.poster_path)}
         alt="poster path of the image"
       />
-      <div className="w-[50%] flex flex-col items-start p-2 gap-2">
+      <div className="w-[80%] lg:w-[50%] flex flex-col items-start p-2 gap-2">
         <h3 className="text-base md:text-xl text-sky-700 font-bold tracking-[1px]">
           {data.original_title}
         </h3>
-
-        <p className="text-[8px] tracking-[0px] md:tracking-[1px] text-gray-200">
-          {data.overview}
-        </p>
 
         <div className="flex items-center gap-2">
           <span className="text-gray-300 text-xs tracking-[0px] md:tracking-[1px]">
