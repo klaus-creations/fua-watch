f8e36bdfb0a90cb61c14bcb94329f552ca7ea780
Abush Natanem <abushnatanem@gmail.com>
2025-03-26 12:10:55 +0300

    update detail page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,39 +1,37 @@
 import { useLoaderData } from "react-router-dom";
-import { tmdbImagePath } from "../utils/imagePath";
 import SmallDescription from "../components/DetailPages/SmallDescription";
 import RecommendationComponent from "../components/DetailPages/RecommendatinoMoviesComponent";
 import ReviewsComponent from "../components/DetailPages/ReviewsComponent";
 
 export default function DetailsPage() {
   const { detailData, recommendation, reviews } = useLoaderData();
+  console.log(detailData);
 
   return (
     <div className="w-full px-4 lg:px-6 flex flex-col gap-5">
       {!detailData ? (
         <div>Loading...</div>
       ) : (
-        <div className="flex flex-col lg:flex-row lg:justify-between w-full">
-          <div className="w-full lg:w-[50%] bg-indigo-950 flex-shrink">
-            <SmallDescription
-              poster={detailData.poster_path}
-              title={detailData.original_name}
-              desc={detailData.overview}
-            />
+        <>
+          <div className="flex flex-col lg:flex-row lg:justify-between w-full">
+            <div className="w-full bg-indigo-950/[.8] flex-shrink">
+              <SmallDescription
+                poster={detailData.poster_path}
+                title={detailData.original_name || detailData.title}
+                desc={detailData.overview}
+                vote={detailData.vote_average}
+                imdb={detailData?.imdb_id}
+                genres={detailData.genres}
+              />
+            </div>
           </div>
-          <div className="w-full lg:w-[50%] bg-yellow-400">
-            <img
-              src={tmdbImagePath(detailData.backdrop_path)}
-              alt="detail page backdrop image"
-              className="w-full"
-            />
-          </div>
-        </div>
-      )}
 
-      <div className="w-full  flex flex-col lg:flex-row justify-between items-start">
-        <RecommendationComponent data={recommendation?.results} />
-        <ReviewsComponent data={reviews?.results} />
-      </div>
+          <div className="w-full  flex flex-col lg:flex-row justify-between items-start">
+            <RecommendationComponent data={recommendation?.results} />
+            <ReviewsComponent data={reviews?.results} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
